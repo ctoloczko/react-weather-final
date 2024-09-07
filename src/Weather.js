e20548a9afe054c955daff01fb42c20540c15825
ctoloczko <celina.toloczko@gmail.com>
2024-09-07 12:51:57 +0200

    added styling to weather component

--- src/Weather.js
+++ src/Weather.js
@@ -1,36 +1,51 @@
 import React from "react";
 import "./Weather.css";
+import WeatherForecast from "./WeatherForecast";
 
 export default function Weather() {
   return (
-    <div>
-      <form>
-        <input type="search" placeholder="Enter a city" />
-        <input type="submit" value="Search" />
-      </form>
+    <>
       <div>
-        <h2>Saturday</h2>
+        <form className="Search-form">
+          <input type="search" placeholder="Enter a city" />
+          <input type="submit" value="Search" />
+        </form>
+        <div className="Current-day-container">
+          <div className="row">
+            <div className="col-7">
+              <h2>Saturday</h2>
+            </div>
+            <div className="col-5 Current-date">
+              <div>7 September</div>
+              <div>8:45AM</div>
+            </div>
+          </div>
+          <div className="City text-center">📍Stockholm</div>
+          <div className="row mt-3">
+            <div className="col text-center">
+              <p className="Icon">🌤️</p>
+              <p>Sunny</p>
+            </div>
+            <div className="col text-center">
+              <div className="Temperature">25°C</div>
 
-        <h4>
-          <span>7 September</span> | <span>8:45AM</span>
-        </h4>
-        <div className="row">
-          <div className="col">
-            <p>🌤️</p>
-            <p>Sunny</p>
+              <p>Feels like: 24° </p>
+            </div>
+            <div className="col Weather-details text-end">
+              <p className="m-0">Humidity: 56% </p>
+              <p className="m-0">Wind: 4m/s </p>
+              <p className="m-0">Pressure: 1020 MB</p>
+            </div>
           </div>
-          <div className="col">
-            <p>25°C</p>
-            <p>Stockholm</p>
-            <p>Feels like: 24° </p>
-          </div>
-        </div>
-        <div>
-          <p>Humidity: 56% </p>
-          <p>Wind: 4m/s </p>
-          <p>Pressure: 1020 MB</p>
         </div>
       </div>
-    </div>
+      <div>
+        <WeatherForecast />
+        <WeatherForecast />
+        <WeatherForecast />
+        <WeatherForecast />
+        <WeatherForecast />
+      </div>
+    </>
   );
 }
---
+++ src/WeatherForecast.js
@@ -0,0 +1,10 @@
+import React from "react";
+export default function WeatherForecast() {
+  return (
+    <div className="Forecast-container">
+      <span>Sunday</span>
+      <span>🌧️</span>
+      <span>10 | 5 °C</span>
+    </div>
+  );
+}
